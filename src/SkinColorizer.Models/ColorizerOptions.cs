4d2f59e18851cd292003ae3a9c0a69ef5c5d2feb
NotArceuid <jaysonchong727@gmail.com>
2022-04-25 20:04:42 +0800

    Removed Unecessary Property

--- src/SkinColorizer.Models/ColorizerOptions.cs
+++ src/SkinColorizer.Models/ColorizerOptions.cs
@@ -1,7 +1,7 @@
 namespace SkinColorizer.Models 
 {
     public class ColorizerOptions
-    {
+    { 
         public float HueDegrees { get; set; }
         public string Path { get; set; }
         public string OutputDirectory { get; set; }
