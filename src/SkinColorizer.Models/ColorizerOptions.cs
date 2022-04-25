034fd0db111be6c7932b0253701a2e7945ba6e5c
NotArceuid <jaysonchong727@gmail.com>
2022-04-25 19:57:32 +0800

    Removed unecessary usings

---
+++ src/SkinColorizer.Models/ColorizerOptions.cs
@@ -0,0 +1,10 @@
+namespace SkinColorizer.Models
+{
+    public class ColorizerOptions
+    {
+        public float HueDegrees { get; set; }
+        public string Path { get; set; }
+        public string OutputDirectory { get; set; }
+
+    }
+}
